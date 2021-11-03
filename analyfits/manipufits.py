23cdb25a27a50546f955f50e56bef71720625216
Igna <ignaao@gmail.com>
2021-11-02 23:28:43 -0300

    major
    refactorizado de codigo para transformar a OOP

--- analyfits/manipufits.py
+++ analyfits/manipufits.py
@@ -3,6 +3,119 @@ from astropy.io import fits
 
 from analyfits import calibration
 
+class ManipulateFits:
+
+    def __init__(self):
+        self.ohdu: int = 0
+        self.alpha: float = calibration.ALPHA
+        self.beta: float = calibration.BETA
+        self.gamma: float = calibration.GAMMA
+        self.delta: float = calibration.DELTA
+
+
+    def set_calibration(self, alpha, beta, gamma, delta):
+        """Sets the desired calibration"""
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.delta = delta
+
+    def set_ohdu(self, ohdu):
+        """Set the desired OHDU to use in the analysis"""
+        self.ohdu = ohdu
+
+    def ADU2e(self, src_path, ADU=False):
+        """
+        From a .fits image, and using the polynomial fit for ADU -> electron
+        returns either both image data:
+            image in ADU's
+            image in e- units.
+        Parameters
+        ----------
+        src_path : string
+            Path to the original .fits file or filename
+        ADU : Bool, optional
+            Default value False, if true is given then returns the numpy array
+            in units of ADU's, otherwise in units of electrons.
+        Returns
+        -------
+        e_img_data
+        """
+        with fits.open(src_path) as fits_img:
+            self.ADU_img_data = fits_img[self.ohdu].data
+            e_original = (
+                self.ADU_img_data * self.alpha
+                + self.ADU_img_data ** 2 * self.beta
+                + self.ADU_img_data ** 3 * self.gamma
+                + self.ADU_img_data ** 4 * self.delta
+            )
+            # e_img_data = e_original
+            self.e_img_data = np.round(e_original)
+            
+            # cleaning of negative values
+            if ADU:
+                self.ADU_img_data[self.ADU_img_data < 0] = 0
+                return self.ADU_img_data
+
+            self.e_img_data[self.e_img_data < 0] = 0
+
+        return self.e_img_data
+
+    def single_fits2double_fits(self, src_path, threshold=1):
+        """
+        From one .fits image generates 2 different images and optionally generates
+        2 new fits files to be saved.
+
+        Parameters
+        ----------
+        src_path : string
+            Path to the original .fits file or filename
+        threshold : int, optional
+            Threshold to start counting electrons. The data will be divided into
+            two diferents arrays, one with all pixels having a number of electrons
+            greater than threshold and the other one having the rest.
+            The default is 1.
+        Returns
+        -------
+        None.
+
+        """
+        electron_fits = self.ADU2e(src_path)
+
+        # make some copies of the original data to modify it avoiding aliasing
+        self.elec_minor = electron_fits.copy()
+        self.elec_major = electron_fits.copy()
+
+        # All pixels with a number of electrons > threshold are set to 0
+        self.elec_minor[elec_minor > threshold] = 0
+        # All pixels with a number of electrons < threshold are set to 0
+        self.elec_major[elec_major <= threshold] = 0
+
+
+        return self.elec_minor, self.elec_major
+
+    def save_fits(self, src_path, tgt_path):
+        """
+        Saves the fits_image data in units of electrons only, into the
+        given tgt_path
+        Parameters
+        ----------
+        tgt_path : str
+            pathname for the new file
+        """
+        with fits.open(src_path) as fits_img:
+            fits_img[self.ohdu].data = self.e_img_data
+            try:
+                fits_img.writeto(tgt_path)
+            except OSError as e:
+                print(e)
+                print("A file with the same name already exists.")
+
+    def easy_plot(self, cmap="hot", vmin=0, vmax=10):
+        """Shortcut to watch the image"""
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(17, 2))
+        plt.imshow(self.e_img_data, cmap=cmap, vmin=vmin, vmax=vmax)
 
 def _fits_saver(fits_image, tgt_path="", ohdu=0):
     """
